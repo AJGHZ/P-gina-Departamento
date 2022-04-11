6d516aa15703d394631cdb63b820783ab66863fe
AJGHZ <aldogarciahernandez@hotmail.com>
2022-04-11 11:44:36 -0500

    Añadiendo codigo slider javascript

---
+++ slider.js
@@ -0,0 +1,19 @@
+(function () {
+    const slider = [...document.querySelectorAll('.slider__body')]; /*Obtiene todos los elementos de la clase*/
+    const arrowNext = document.querySelector('#next');
+    const arrowBefore = document.querySelector('#before');
+    let value;
+
+    arrowNext.addEventListener('click', () => changePosition(1));
+
+    arrowBefore.addEventListener('click', () => changePosition(-1));
+
+    function changePosition(change) {
+        console.log(change);
+        const currentElement = Number(document.querySelector('.slider__body--show').dataset.id);
+        
+        value  = currentElement;
+        value+= change;
+    
+    }
+})()
